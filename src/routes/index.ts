73eba7a3f24e8af55a09558bcfc321e38d1efaec
martinkassar <40896885+MartinKassar@users.noreply.github.com>
2019-06-10 11:08:25 +0200

    post and get

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 const router = Router();
 import { users } from './methods.js';
 
-router.get("/user/:id", users.get);
+router.get("/user/:id", users.getUser);
+router.get('/quiz', users.getQuiz);
+router.post('/quiz', users.createQuiz);
 
 export default router;
--- src/routes/methods.ts
+++ src/routes/methods.ts
@@ -1,4 +1,5 @@
 import { UserModel } from "../schemas/user";
+import { QuizModel } from "../schemas/quiz"
 
 let getUser = async (req: any, res: any, next: any) => {
     try {
@@ -11,7 +12,62 @@ let getUser = async (req: any, res: any, next: any) => {
         res.status(500).send(error);
     }
 }
+let createUser = async (req: any, res: any, next: any) => {
+    try {
+        const result = await UserModel.create({
+            userId: req.body.userId,
+            email: req.body.email
+        });
+        if (result) {
+            res.status(200).send(result);
+        }
+        else {
+            throw "Couldn't create user!";
+        }
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+}
+let getQuiz = async (req: any, res: any, next: any) => {
+    try {
+        const result = await QuizModel.findById(req.params.id).exec();
+        if (result) {
+            res.status(200).send(result);
+        }
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+
+let createQuiz = async (req: any, res: any, next: any) => {
+    try {
+        const result = await QuizModel.create({
+            name: req.body.name,
+            user: req.body.user,
+            questions: [{
+                question: req.body.questions.question,
+                answer: req.body.questions.answer
+            }]
+        })
+        if (result) {
+            res.status(200).send(result);
+        }
+        else {
+            throw "Couldn't create quiz!";
+        }
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+
 
 export const users = {
-    get: getUser
+    getUser: getUser,
+    getQuiz: getQuiz,
+    createQuiz: createQuiz
 };
